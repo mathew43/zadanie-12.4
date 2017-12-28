08dd8ef10ff669d58600025fe6179fa389858a4b
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-28 23:16:58 +0100

    delete unnessesary elements

--- App.js
+++ App.js
@@ -1,22 +1,3 @@
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
-
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
 	'X-Client-Id': 'X-Client-Id',
